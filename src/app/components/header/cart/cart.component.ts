74788d2e452b470292410ab8bc221f1441d70d83
bugong <1761869346@qq.com>
2017-09-01 22:58:10 +0800

    修改cart

--- src/app/components/header/cart/cart.component.ts
+++ src/app/components/header/cart/cart.component.ts
@@ -1,14 +1,30 @@
 import {Component, OnInit, ViewEncapsulation} from "@angular/core";
 
+import {CartService} from './cart.service';
+
 @Component({
     encapsulation: ViewEncapsulation.None,
     selector: 'header-cart',
     templateUrl: './cart.html',
-    styleUrls: ['./cart.scss']
+    styleUrls: ['./cart.scss'],
+    providers: [CartService]
 })
 export class CartComponent implements OnInit {
 
+    orders: Array<any> = [];
+
+    constructor(
+        private cartService: CartService
+    ) {
+    }
+
     ngOnInit() {
 
+        this.orders = this.cartService.getOrders();
+    }
+
+    deleteOrder(orderId: string | number) {
+
+        this.cartService.deleteOrder(orderId);
     }
 }
---
+++ src/app/components/header/cart/cart.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+
+import {BaseHttpService, BaseHttpOptions} from '../../../services/base-http.service';
+
+@Injectable()
+export class CartService extends BaseHttpService {
+
+    getOrders(): Array<any> {
+        return [{
+            id: 101,
+            partCode: 'B00004034',
+            partName: '六角法兰面螺栓',
+            num: 1
+        }, {
+            id: 102,
+            partCode: 'C00064853',
+            partName: 'ECU支架',
+            num: 1
+        }, {
+            id: 103,
+            partCode: 'C00094027',
+            partName: '变速器操纵杆手柄',
+            num: 1
+        }, {
+            id: 104,
+            partCode: 'C00102782',
+            partName: '变速器操纵杆防尘罩',
+            num: 4
+        }];
+    }
+
+    deleteOrder(orderId: string | number) {
+
+        console.log(orderId);
+    }
+}
