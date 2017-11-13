ca25519cf969a50e8a5f9d103c323a30cb259ad7
bugong <1761869346@qq.com>
2017-11-13 21:41:45 +0800

    提交修改

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy} from '@angular/core';
 
 @Component({
     encapsulation: ViewEncapsulation.None,
     selector: 'app-footer',
     templateUrl: './footer.html',
-    styleUrls: ['./footer.scss']
+    styleUrls: ['./footer.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FooterComponent implements OnInit {
 
